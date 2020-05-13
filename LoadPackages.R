c6c2703fba15715778cf74a5216fb4e335620c27
danieltompsett <64539569+danieltompsett@users.noreply.github.com>
2020-05-13 13:36:23 +0100

    LoadPackages.R
    Script to load the required packages for which gesttools depends on.

---
+++ LoadPackages.R
@@ -0,0 +1,20 @@
+#The functions of gesttools require the following packages
+#Note that an additional argument "lib" may be required
+#to specify the file path to your R installation 
+#For example
+
+#install.packages("packagename",lib="filepath")
+
+install.packages("DataCombine") #slide function
+install.packages("tidyr") #nest_legacy function for bootstrap
+install.packages("dplyr") #tbl 
+install.packages("tibble") #as_tibble function
+install.packages("geeM") #geem function
+install.packages("rsample") #bootstraps function
+
+library(DataCombine)#slide function
+library(tidyr)#nest_legacy function for bootstrap
+library(dplyr)#tbl 
+library(tibble)#as_tibble function
+library(geeM)# geem function
+library(rsample)# bootstrap
